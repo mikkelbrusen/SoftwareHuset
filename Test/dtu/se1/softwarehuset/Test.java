a60f35737473bfee74af8c8eb72b43c99949a39e
Jeppe Enslev <jev@umwelt.dk>
2015-04-13 16:10:07 +0200

    Fix various

---
+++ Test/dtu/se1/softwarehuset/Test.java
@@ -0,0 +1,4 @@
+package dtu.se1.softwarehuset;
+public class Test {
+
+}
---
+++ src/dtu/se1/softwarehuset/Master.java
@@ -0,0 +1,7 @@
+package dtu.se1.softwarehuset;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Master {
+}
