492685233adde06f147bb6a00e535ba8f82cbeeb
Tobias Slott Jensen <tobias.slott@post.tele.dk>
2015-05-07 17:19:53 +0200

    Add: AlreadyExistingException class

---
+++ Test/dtu/se1/softwarehuset/AlreadyExistingException.java
@@ -0,0 +1,13 @@
+package dtu.se1.softwarehuset;
+
+public class AlreadyExistingException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AlreadyExistingException(String msg) {
+		super(msg);
+	}
+}
